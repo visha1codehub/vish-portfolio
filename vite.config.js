199df59cea206023c88916550f71406c64029dc3
Vishal Gupta <itsvishalhack@gmail.com>
2025-07-01 23:49:32 +0530

    add redirect conf for render.com

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    historyApiFallback: true, // <-- dev mode fallback
+  },
+  build: {
+    outDir: 'dist',
+  },
+  preview: {
+    // needed for 'vite preview' or static hosting
+    historyApiFallback: true,
+  },
 })
